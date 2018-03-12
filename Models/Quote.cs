163c64b922465ef5f683fe162ea5b002cb57c891
Patti <Jones-Patti@users.noreply.github.com>
2018-03-12 17:35:35 -0600

    Edit file and updated Delete

--- Models/Quote.cs
+++ Models/Quote.cs
@@ -12,32 +12,34 @@ namespace MegaDeskMVC.Models
         [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}")]
         public DateTime Date { get; set; }
 
-
+        [Display(Name = "First Name")]
         [MinLength(2)]
         [MaxLength(20)]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last Name")]
         [MinLength(2)]
         [MaxLength(20)]
         public string LastName { get; set; }
 
+        [Display(Name = "Desk Width")]
         public double DeskWidth { get; set;  }
+
+        [Display(Name = "Desk Length")]
         public double DeskLength { get; set; }
         public int Drawers { get; set;  }
         public string Material { get; set;  }
 
-
-        [DisplayFormat(DataFormatString = "{0:C}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Material Price")]
         public double MaterialPrice { get; set;  }
 
+        [Display(Name = "Shipping Days")]
         public int ShippingDays { get; set; }
 
-        [DataType(DataType.Currency)]
-        [DisplayFormat(DataFormatString = "{0:C0}")]
+        [Display(Name = "Shipping Price")]
         public double ShippingPrice { get; set;  }
 
-        [DisplayFormat(DataFormatString = "{0:C0}")]
-
+        [Display(Name = "Quote Amount")]
         [Range(0, 50000)]
         public double Amount { get; set;  }
 
